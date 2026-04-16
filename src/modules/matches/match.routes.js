7ea750d8bfeaacdb6e5647e696b4d308d808164a
Cristian Zavaleta <cristian.zavaleta.b@tecsup.edu.pe>
2026-04-16 10:01:47 -0500

    Ultra-Pro Centered UI, 404 Route Fix, and Image Path Correction

--- src/modules/matches/match.routes.js
+++ src/modules/matches/match.routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./match.controller');
 
+router.get('/', controller.getAllActiveMatches);
 router.get('/live', controller.getLiveMatches);
 router.get('/active', controller.getAllActiveMatches);
 router.get('/:id', controller.getMatchDetails);
